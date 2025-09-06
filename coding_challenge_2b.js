7392237b1930d468d4d9442f69bc48bc6927ddef
Kameron Thibault <kameronthibault@usf.edu>
2025-09-06 19:24:31 -0400

    initial portfolio page

--- coding_challenge_2b.js
+++ coding_challenge_2b.js
@@ -10,7 +10,7 @@ supplierLeadTimeWeeks = "2 week"
 weeksOfCover = (weeklyDemand > 0 ? currentStock / weeklyDemand : Infinity), round = 2
 stockDeficit = Math.max(0, targetStock - currentStock)
 reorderQuantity = (currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks) ? Math.ceil(stockDeficit) : 0
-estimatedReorderCost = currency-like.toFixed(2)(reorderQuantity * unitCost)
+estimatedReorderCost = (reorderQuantity * unitCost.tofixed(2))
 reorderNow = "REORDER NOW", currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks
 
 console.log (itemname)
